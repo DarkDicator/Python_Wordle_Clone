4e20dc379a5fe031de6ee28f95978d70f3d5024d
Davin <davinmailsklh@gmail.com>
2022-08-24 14:08:49 +0700

    Python Wordle

---
+++ Wordle.py
@@ -0,0 +1,32 @@
+import random
+from termcolor import colored
+
+
+words = list(open("words.txt", "r"))
+word = random.choice(words).strip()
+display = ["_", "_", "_", "_", "_"]
+win = False
+
+
+for i in range(6):
+    print("".join(display))
+    display = ["_", "_", "_", "_", "_"]
+    answer = ""
+    print(word)
+    while answer + "\n" not in words:
+        answer = input("Input your guess: ")
+    if answer == word:
+        win = True
+        break
+    else:
+        for letter in range(5):
+            if answer[letter] == word[letter]:
+                display = list(display)
+                display[letter] = colored(word[letter], "green")
+            else:
+                if answer[letter] in word:
+                    display[letter] = colored(answer[letter], "yellow")
+                    
+
+
+print("You win!") if win == True else print("You lose, The word was: " + word)
